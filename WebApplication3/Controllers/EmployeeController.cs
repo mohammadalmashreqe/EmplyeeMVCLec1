2c1363d69d729255764d0f7073aa5c813999a7dd
MOHAMMAD MUSHRIQI <mymushriqi15@cit.just.edu.jo>
2019-07-20 13:00:36 +0300

    Add project files.

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       public static  List<Models.Employee> EmpList = new List<Models.Employee>();
+        // GET: Employee
+        public ActionResult Index()
+        {
+
+           
+         
+            return View(EmpList);
+        }
+        public ActionResult Create ()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Employee e)
+        {
+            EmpList.Add(e);
+           return  RedirectToAction("Index");
+        }
+        public ActionResult Details(int id )
+        {
+            Models.Employee e1 = EmpList.Where(e => e.ID == id).FirstOrDefault();
+                                
+
+            return View(e1);
+        }
+        public ActionResult Edit (int id )
+        {
+            Models.Employee e1 = EmpList.Where(e => e.ID == id).FirstOrDefault();
+
+
+            return View(e1);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Models.Employee st)
+        {
+            int index = -1; 
+            for(int i=0; i<EmpList.Count;i++)
+            {
+                if (EmpList[i].ID == st.ID)
+                    index = i;
+            }
+
+            if(index!=-1)
+            {
+                EmpList[index] = st;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication3/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public int ID { set; get; }
+
+        public string Name { set; get; }
+
+        public int Age { set; get;  }
+
+        public double Sallary { set; get; }
+    }
+}
